80dd7e1be4a828e6fbfb3da047f59bc1bf22adb4
Iván López <lopezi@objectcomputing.com>
2021-05-13 09:49:50 +0200

    Only use workaround for kapt with JDK 16

--- build-native-image.sh
+++ build-native-image.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+JDK_VERSION=`java -version 2>&1 | awk -F '"' '/version/ {print $2}'`
+JDK_16='16'
+
+if [ "${JDK_VERSION}" != "${JDK_VERSION%"$JDK_16"*}" ]; then
+  export GRADLE_OPTS="-Dorg.gradle.jvmargs=--illegal-access=permit"
+fi
+
 ./gradlew nativeImage
 cp build/native-image/kotlin .
