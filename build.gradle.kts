29e5501808dcc5f34844ac556c5c3cac9d867d45
Iván López <lopezi@objectcomputing.com>
2021-06-24 11:04:55 +0200

    Upgrade Micronaut Gradle plugin to 1.5.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.kapt") version "1.4.30"
     id("org.jetbrains.kotlin.plugin.allopen") version "1.4.30"
     id("com.github.johnrengelman.shadow") version "6.1.0"
-    id("io.micronaut.application") version "1.5.0"
+    id("io.micronaut.application") version "1.5.3"
 }
 
 version = "0.1"
