1d1db9675bdd4a8e8ee0bfb31caa17112d3f9b09
Iván López <lopezi@objectcomputing.com>
2021-07-02 16:02:44 +0200

    Upgrade Micronaut Gradle plugin to 2.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.kapt") version "1.4.32"
     id("org.jetbrains.kotlin.plugin.allopen") version "1.4.32"
     id("com.github.johnrengelman.shadow") version "7.0.0"
-    id("io.micronaut.application") version "2.0.1"
+    id("io.micronaut.application") version "2.0.2"
 }
 
 version = "0.1"
