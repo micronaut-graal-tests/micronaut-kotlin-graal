5e2fdebc1f67e92a12d1eb320eaf3dd32f6aebf2
Iván López <lopezi@objectcomputing.com>
2021-09-07 16:45:18 +0200

    Add workaround for JDK17 and Kapt
    See https://youtrack.jetbrains.com/issue/KT-45545

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,3 +60,22 @@ tasks {
         setImageName("kotlin")
     }
 }
+
+if (JavaVersion.current().isCompatibleWith(JavaVersion.VERSION_16)) {
+    // Workaround for https://youtrack.jetbrains.com/issue/KT-45545
+    kotlin {
+        kotlinDaemonJvmArgs = listOf(
+            "-Dfile.encoding=UTF-8",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.jvm=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED"
+        )
+    }
+}
